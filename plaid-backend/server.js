7e1165597f082277e3fe22147a7794e919dbb792
Higher Star <goldbyol@outlook.com>
2019-07-22 12:08:51 +0300

    Update server.js

--- plaid-backend/server.js
+++ plaid-backend/server.js
@@ -43,3 +43,15 @@ app.listen(port, () => {
   //   }
 	// );
 });
+
+app.use(express.static(path.resolve(__dirname, "../plaid-frontend/", "build")));
+app.use("/static", express.static(path.resolve(__dirname, "..", "static")));
+
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../plaid-frontend/', 'build', 'index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    });
+});
