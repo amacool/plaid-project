ff8948c90578914190971c3c85d90ef2ffa9c14c
Higher Star <goldbyol@outlook.com>
2019-07-22 10:09:56 +0300

    Update constants.js

--- plaid-frontend/src/redux/constants.js
+++ plaid-frontend/src/redux/constants.js
@@ -1,3 +1,3 @@
 export const plaidPublicKey = 'aeec6a4876fd99bb7e26c51c8d3217';
-export const urlPrefix = "http://localhost:9000/";
+export const urlPrefix = "https://secure.fundingtree.io:9000/";
 //export const urlPrefix = "http://165.22.57.105:9000/";
