be4fc993b8fb3f11594b8d51e55a0eaf3255ef41
Gold Byol <goldbyol@outlook.com>
2019-08-28 23:44:58 +0300

    update backend path

--- plaid-frontend/src/redux/constants.js
+++ plaid-frontend/src/redux/constants.js
@@ -1,3 +1,3 @@
 export const plaidPublicKey = 'aeec6a4876fd99bb7e26c51c8d3217';
-export const urlPrefix = "https://api.theleadfeed.ai/";
+export const urlPrefix = "https://api.fundingtree.io/";
 // export const urlPrefix = "http://localhost:9000/";
