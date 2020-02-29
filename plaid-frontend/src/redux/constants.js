5175f483ac27f29698f8dae9bcc9cd3ad1dc2059
Cool Dev <49298234+amacool@users.noreply.github.com>
2020-02-29 23:10:34 +0100

    Update constants.js

--- plaid-frontend/src/redux/constants.js
+++ plaid-frontend/src/redux/constants.js
@@ -1,3 +1,2 @@
-export const plaidPublicKey = 'aeec6a4876fd99bb7e26c51c8d3217';
-export const urlPrefix = "https://api.fundingtree.io/";
-// export const urlPrefix = "http://localhost:9000/";
+export const plaidPublicKey = '';
+export const urlPrefix = "";
